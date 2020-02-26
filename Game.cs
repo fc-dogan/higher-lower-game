cb3bc84d00fa34bfc1ec24f6f1cebec72fada849
Fatma <fceyda@gmail.com>
2020-02-25 22:19:55 -0800

    add Game class

--- Game.cs
+++ Game.cs
@@ -1,67 +1,73 @@
 using System;
-using System.Collections.Generic;
+
+public class Game
+{
+  public int MinNumber;
+  public int MaxNumber;
+
+  public bool PlayGame;
+
+  public Game(int minNumber, int maxNumber, bool playGame)
+  {
+    MinNumber = minNumber;
+    MaxNumber = maxNumber;
+    PlayGame = playGame;
+  }
+
+
+
+}
+
 
 public class Program
 {
 
+public static int RandomNumber(int minNumber, int maxNumber)
+{
+  var rand = new Random();
+  int index = rand.Next(minNumber, maxNumber);
+  return index;
+}
   
 
-  public int GuessNumber (int index)
+  public static void GuessNumber (int index)
   {
     Console.WriteLine("Is your number higher or lower than " + index + "? (Higher/Lower/Correct");
       string answer = Console.ReadLine();
-      var rand = new Random();
+     
     if( answer == "Higher")
     {
-      index = rand.Next(index, 101);
-      return index;
+      Game newGame = new Game(index, 101, true);
+      int randomNumber = RandomNumber(newGame.MinNumber, newGame.MaxNumber);
+      GuessNumber(randomNumber);
 
     }
     else if ( answer == "Lower")
     {
-      index = rand.Next(index);
-      return index;
+      Game newGame = new Game(0, index, true);
+      int randomNumber = RandomNumber(newGame.MinNumber, newGame.MaxNumber);
+      GuessNumber(randomNumber);
     }
     else
     {
-      // string response = "Great! I guessed your number.";
-      return  -1;
+      Console.WriteLine( "Great! I guessed your number.");
+     
     }
   }
 
   public static void Main()
   {
+   
     Console.WriteLine("Would you like to play the highger/lower game?(Yes/No)");
     string playGame = Console.ReadLine();
 
     if (playGame == "Yes")
     {
-      Console.WriteLine("Pick a number between 1-100");
-      var rand = new Random();
-      int index = rand.Next(101);
-
-      GuessNumber(index);
+      Console.WriteLine("Pick a number between 1-100"); 
+      Game newGame = new Game(0, 101, true);
+      int randomNumber = RandomNumber(newGame.MinNumber, newGame.MaxNumber);
   
-      // Console.WriteLine("Is your number higher or lower than " + index + "? (Higher/Lower/Correct");
-      // string answer = Console.ReadLine();
-      
-      //   if( answer == "Higher")
-      //   {
-      //     index = rand.Next(index, 101);
-      //     return index;
-
-      //   }
-      //   else if ( answer == "Lower")
-      //   {
-      //     index = rand.Next(index);
-      //     return index;
-      //   }
-      //   else
-      //   {
-      //     string response = "Great! I guessed your number.";
-      //     return  response;
-      //   }
-          
+      GuessNumber(randomNumber);
     }
     else
     {
